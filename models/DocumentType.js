53732320f79b09a954634b6113743f1fa0771cd7
Varun Tanna <varun_tanna@ymail.com>
2022-07-05 14:16:04 -0400

    adding init.js to config folder

--- models/DocumentType.js
+++ models/DocumentType.js
@@ -3,25 +3,24 @@ const { Model, DataTypes } = require('sequelize');
 class DocumentType extends Model {}
 
 DocumentType.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'DocumentType',
-    }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'DocumentType',
+  }
 );
 
 module.exports = DocumentType;
-
--- models/Trip.js
+++ models/Trip.js
@@ -3,40 +3,39 @@ const { Model, DataTypes } = require('sequelize');
 class Trip extends Model {}
 
 Trip.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        date_start: {
-            type: DataTypes.DATE,
-            allowNull: false,
-        },
-        date_end: {
-            type: DataTypes.DATE,
-            allowNull: false,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id',
-            }
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'Trip',
-    }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_start: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    date_end: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Trip',
+  }
 );
 
 module.exports = Trip;
-
