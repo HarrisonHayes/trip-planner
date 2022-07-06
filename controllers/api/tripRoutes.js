5ed996c795efded5fa5e5886d26e754115f6b743
Chris Okamuro <cokamuro@gmail.com>
2022-07-06 12:59:00 -0400

    more fixes to routes

--- controllers/api/tripRoutes.js
+++ controllers/api/tripRoutes.js
@@ -2,22 +2,26 @@ const router = require("express").Router();
 const { Trip } = require("../../models");
 const isAuth = require("../../utils/auth");
 
-
-
-router.post("/", async (req, res) => {
-    if (req.session.loggedIn = true) {
-        try {
-            const tripData = await Trip.create({
-                name: req.body.name,
-                date_start: req.body.date_start,
-                date_end: req.body.date_end
-            });
-            console.log(tripData)
-            res.status(200).json(tripData);
-        }
-        catch (err) {
-            console.log(err)
-            res.status(400).json(err);
-        }
+// create new trip
+router.post("/trip", async (req, res) => {
+    try {
+      const tripData = await Trip.create({
+        name: req.body.name,
+        start_date: req.body.start_date,
+        end_date: req.body.end_date,
+        user_id: req.session.user_id,
+      });
+      const trip = tripData.get({ plain: true });
+      if (tripData) {
+        res.status(201).json({ id: trip.id });
+      } else {
+        res
+          .status(500)
+          .json({ message: "There was an error while creating the trip" });
+      }
+    } catch (err) {
+      res.status(500).json(err);
     }
-});
+  });
+
+  module.exports = router;
--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -9,27 +9,7 @@ const {
 
 
 
-// create new trip
-router.post("/trip", async (req, res) => {
-  try {
-    const tripData = await Post.create({
-      name: req.body.name,
-      start_date: req.body.start_date,
-      end_date: req.body.end_date,
-      user_id: req.session.user_id,
-    });
-    const trip = tripData.get({ plain: true });
-    if (tripData) {
-      res.status(201).json({ id: trip.id });
-    } else {
-      res
-        .status(500)
-        .json({ message: "There was an error while creating the trip" });
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+
 
 // update a trip by id
 router.put("/trip/:id", async (req, res) => {
