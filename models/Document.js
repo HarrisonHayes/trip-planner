832ae0be6f30d6113c291d8f6e77ed9cec456ac7
Sela Privette <selaprivette@yahoo.com>
2022-07-10 22:25:17 -0400

    stage for pull

--- models/Document.js
+++ models/Document.js
@@ -1,45 +1,45 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
 class Document extends Model {}
 
 Document.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        content: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        type_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'documentType',
-                key: 'id',
-            }
-        },
-        destination_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'destination',
-                key: 'id',
-            }
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'document',
-    }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    type_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'documentType',
+        key: 'id',
+      },
+    },
+    destination_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'destination',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'document',
+  }
 );
 
 module.exports = Document;
-
--- models/DocumentType.js
+++ models/DocumentType.js
@@ -1,27 +1,27 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
 class DocumentType extends Model {}
 
 DocumentType.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'DocumentType',
-    }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'documentType',
+  }
 );
 
 module.exports = DocumentType;
-
